afb3bf07e2c84f6d8424269861842cdb9cc2977e
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2024-10-23 16:57:46 +0700

    add zullip features

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from datetime import datetime
+import json
+
+from kafka import KafkaConsumer
 from connect_db.connect import connect_db
 from module.tbmt import Tbmt
 from zullip_module import zullip_code as zc
 from logger import get_app_logger
 
+def getdecode(obj):
+    if obj is None:
+        return None
+    else:
+        return json.loads(obj.decode('utf-8'))
 
 def table_to_send(tbmts: list[Tbmt]) -> str:
     msg = f'''
@@ -41,11 +49,45 @@ def send_tbmt_zullip():
     tbmts: list[Tbmt] = cursor.fetchall()
     if len(tbmts) > 0:
         msg_task = task_to_send(tbmts)
-        result = zc.send_msg(msg_task, 1974)
+        result = zc.send_msg_group(msg_task, 1974)
         logger.info(result)
     else:
         logger.info("No more TBMT to send today.")
 
+def kafka_consumer():
+    logger = get_app_logger()
+    conn = connect_db()
+    cursor = conn.cursor()
+    bootstrap_server = '192.168.100.52:9092'
+    topic = 'notify.SKMT_App.dbo.notify_zullip'
+    # To consume latest messages and auto-commit offsets
+    consumer = KafkaConsumer(
+                            topic,
+                            client_id='zullip-local',
+                            group_id='zullip-consumer',
+                            bootstrap_servers=bootstrap_server,
+                            auto_offset_reset='earliest',
+                            value_deserializer=lambda m: getdecode(m)
+                            )
+    try:
+        for message in consumer:
+            msg_before = message.value['payload']['before']
+            msg = message.value['payload']['after']
+            logger.info(msg['content'])
+            if msg_before == None:
+                if (msg['group_yn'] != 1 and msg['to_person'] != 64):
+                    result = zc.send_msg_private(msg['content'], int(msg['to_person']))
+                else:
+                    result = zc.send_msg_group(msg['content'], int(msg['to_person']))
+                if result['result'] == 'success':
+                    sql_query = 'update notify_zullip set datetime2= getdate(), status = 1 where id = ' + str(msg['id'])
+                    cursor.execute(sql_query)
+                    conn.commit()
+                logger.info(result)
+    except Exception as e:
+        conn.rollback()
+        logger.error("ERROR ", e)
+    conn.close()
 
 if __name__ == '__main__':
-    send_tbmt_zullip()
+    kafka_consumer()
--- zullip_module/zullip_code.py
+++ zullip_module/zullip_code.py
@@ -6,11 +6,22 @@ import markdown
 client = zulip.Client(config_file=os.path.dirname(__file__)+"/zuliprc")
 
 
-def send_msg(msg: str, to: int) -> any:
+def send_msg_group(msg: str, to: int) -> any:
     request = {
         "type": "stream",
         "to": to,
         "topic": "channel events",
+        "content": markdown.markdown(msg).replace("<p>", "").replace("</p>", "").replace("<pre><code>", "").replace(
+            "</code></pre>", ""),
+    }
+    result = client.send_message(request)
+    return result
+
+
+def send_msg_private(msg: str, to: int) -> any:
+    request = {
+        "type": "private",
+        "to": [to],
         "content": markdown.markdown(msg).replace("<p>", "").replace("</p>", "").replace("<pre><code>", "").replace(
             "</code></pre>", ""),
     }
